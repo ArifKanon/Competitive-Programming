050189384fd5f44358fb36e9462d1503ad7549ba
arif <mdarifkanon@gmail.com>
2023-01-10 08:28:25 +0600

    Vowel check using regex

--- common-problems/CheckVowelInString.java
+++ common-problems/CheckVowelInString.java
@@ -1,15 +1,23 @@
 public class CheckVowelInString {
     
+    /*
+     * Check vowel is present in String way 1: using regular expression
+     */
     private static boolean checkVowelIsPresent(String str) {
-
-        for(int i = 0; i < str.length(); i++) {   
-            char letter = Character.toLowerCase(str.charAt(i));
-            if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
-                return true;  
-        }
-
-        return false;
+        return str.toLowerCase().matches(".*[aeiou].*");
     }
+
+    /*
+     * Check vowel is present in String way 2: character checking 1 by 1
+     */
+    // private static boolean checkVowelIsPresent(String str) {
+    //     for(int i = 0; i < str.length(); i++) {   
+    //         char letter = Character.toLowerCase(str.charAt(i));
+    //         if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+    //             return true;  
+    //     }
+    //     return false;
+    // }
 
     public static void main(String args[]) {        
         String str = "Happy Coding";
